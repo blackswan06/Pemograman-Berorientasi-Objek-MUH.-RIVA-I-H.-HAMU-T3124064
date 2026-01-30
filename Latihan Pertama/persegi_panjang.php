f6980aaeddce5bd3bc1aecaf4b80fe9957d34bff
unknown <muhrifai682@gmail.com>
2026-01-29 23:47:54 -0800

    latihan 1

--- Latihan Pertama/persegi_panjang.php
+++ Latihan Pertama/persegi_panjang.php
@@ -23,9 +23,17 @@ $pp2 = new PersegiPanjang();
 $pp2->panjang = 7;
 $pp2->lebar = 3;
 
-//cetek
+//cetak
 echo "<h3>Persegi Panjang 1</h3>";
-echo "Panjang : {$pp1->panjang}<br>";
-echo "Lebar : {$pp1->lebar}<br>";
+echo "panjang: " . $pp1->panjang . "<br>";
+echo "lebar: " . $pp1->lebar . "<br>";
+echo "Luas: " . $pp1->hitungluas() . "<br>";
+echo "Keliling: " . $pp1->hitungkeliling() . "<br>";
+
+echo "<h3>Persegi Panjang 2</h3>";
+echo "panjang: " . $pp2->panjang . "<br>";
+echo "lebar: " . $pp2->lebar . "<br>";
+echo "Luas: " . $pp2->hitungluas() . "<br>";
+echo "Keliling: " . $pp2->hitungkeliling() . "<br>";
 
 ?>
